a92d70c3a64c316e467f05e7ca9713610618ed86
René Mose Hansen <rmha63250@edu.ucl.dk>
2025-04-10 12:50:10 +0200

    fixed

--- Lommeregner/Calculator.cs
+++ Lommeregner/Calculator.cs
@@ -4,7 +4,7 @@ namespace Lommeregner
 {
     public class Calculator : ICalculator
     {
-        public int Add(int a, int b) => a + b + b;
+        public int Add(int a, int b) => a + b;
         public int Subtract(int a, int b) => a - b;
         public int Multiply(int a, int b) => a * b;
         public double Divide(int a, int b)
